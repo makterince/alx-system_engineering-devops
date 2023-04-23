9d485eb14296873100506ba1198eb49e665fcc8a
makterince <fomaku3@gmail.com>
2023-04-23 07:24:11 -0700

    ssh req

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 #using puppet to configure ssh requirements
 
-file { '/etc/ssh/sshd_config':
-  ensure => present,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  content => "
-    # SSH client configuration
-    Host school
-    HostName <server_hostname>
-    User <username>
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-  ",
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
