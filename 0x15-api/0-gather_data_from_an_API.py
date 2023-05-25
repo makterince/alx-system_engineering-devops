23f281aa32edbd487f73eb30c1c3ef7517d2aac1
Festus Omaku <fomaku3@gmail.com>
2023-05-25 20:03:14 +0000

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-""" scripts returns info about emplyee """
-import requests
+import request
 
-def get_employee_details(employee_id):
-    """ make api request """
-    response = request.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+def employee_details(employee_id):
+	base_url = "https;//jsonplaceholder.typicode.com"
+	employee_url =  f"{base_url}/users/{employee_id}"
+	todos_url = f"{base_url}/todos?userId={employee_id}"
 
-    if response.status_code == 200:
-        todos = response.json()
+	try:
+	    employee_response = request.get(employee_url)
+	    employee_response.raise_for_status()
+    	    employee_data = employee_response.json()
+        
+            todos_response = requests.get(todos_url)
+            todos_response.raise_for_status()
+            todos_data = todos_response.json()
 
-        completed_tasks = [todo for todo in todos if todo['completed']]
-        total_tasks = len(todos)
-        num_completed_tasks = len(completed_tasks)
+	    completed_tasks = [task for task in todos_data if task['completed']]
+            employee_name = employee_data['name']
+            total_tasks = len(todos_data)
+            done_tasks = len(completed_tasks)
+            print(f"Employee {employee_name}
+		  is done with tasks({done_tasks}/{total_tasks}):")
 
-        employee_name = todos[0]['username']
-
-        print(f'Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):')
-
-        for task in completed_tasks:
-            print("\t", task['title'])
-    else:
-        print(f"Error: {response.status_code}")
+	    for task in completed_tasks:
+           	 print(f"\t{task['title']}")
+        
+        except requests.exceptions.RequestException as e:
+	        print(f"An error occurred: {e}")
 
 employee_id = int(input("Enter the employee ID: "))
 get_employee_todo_progress(employee_id)
