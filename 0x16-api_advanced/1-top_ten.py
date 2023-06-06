84513cc20058f0dc1961e154dd7050577e61746f
Festus Omaku <fomaku3@gmail.com>
2023-06-06 22:50:57 +0100

    hot 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests
+""" gets the first 10 hot posts listed for a subreddit """
+
+def top_ten(subreddit):
+    """ gets the 10 hot posts """
+
+    headers = {"User-Agent": "My Reddit API Client"}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data["data"]["children"]
+            for post in posts[:10]:
+                print(post["data"]["title"])
+        elif response.status_code == 404:
+            print("None")
+        else:
+            print("None")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
