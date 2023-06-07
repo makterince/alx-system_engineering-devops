e2813b0a360e2a348a5f56d9857b6903583094cf
Festus Omaku <fomaku3@gmail.com>
2023-06-07 02:12:12 +0100

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """ rerturns number of subscribers """
+
 import requests
+
+
 def number_of_subscribers(subreddit):
     """ checks and returns total number of subscribers """
 
 
     headers = { 'User-Agent': 'testing' }
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data["data"]["subscribers"]
-        elif response.status_code == 404:
-            return 0
-        else:
-            return 0
-    except requests.exceptions.RequestException as e:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data("data")("subscribers")
+    else:
         return 0
