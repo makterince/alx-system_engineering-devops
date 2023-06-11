fdde6774310e15a4fe7bdc4635d5b33b2c7fcbb0
makterince <fomaku3@gmail.com>
2023-06-11 11:07:58 -0700

    trying

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,5 @@
 # Install required packages or dependencies
-package { 'package_name':
-  ensure => 'installed',
-}
-
-# Set correct file permissions
-file { '/path/to/file':
-  ensure  => 'file',
-  owner   => 'apache',
-  group   => 'apache',
-  mode    => '0644',
-  content => template('module_name/file_template.erb'),
-  notify  => Service['apache'],
-}
-
-# Restart Apache service
-service { 'apache':
-  ensure  => 'running',
-  enable  => true,
-  require => File['/path/to/file'],
+exec {'searchAndReplace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
